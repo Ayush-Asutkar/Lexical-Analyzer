8eeba00e808a242d4d857d26ac9f7e5846ce28da
Ayush-Asutkar <asutkarayush@gmail.com>
2023-08-03 16:42:07 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    private static String readInputString() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        sc.close();
+        return input;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Provide the input line: ");
+        String input = readInputString();
+        System.out.println("The read input is: " + input);
+    }
+}
